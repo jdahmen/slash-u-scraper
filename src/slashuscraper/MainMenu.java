6d648a07f3174cf6d8a8e2903ce977fc540d0e46
jdahmen <jordandahmen@gmail.com>
2015-04-16 08:17:14 -0400

    Completed file reading, user input reading and general error handing for the "MainMenu". Still needs scarper additions and maybe some indicators

--- src/slashuscraper/MainMenu.java
+++ src/slashuscraper/MainMenu.java
@@ -1,5 +1,10 @@
 package slashuscraper;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,19 +17,21 @@ public class MainMenu {
 	// main method
 	public static void Main(String[] args) {
 		
-		String input; // user input
-		
-		ArrayList<String> usernames; // list of usernames
+		String input; 											// user input		
+		ArrayList<String> usernames = new ArrayList<String>(); 	// list of usernames
 		
 		// user arguments found
 		if(args.length > 0) {
 			// argument is file parameter
 			if(args[0].trim().equals("-f") || args[0].trim().equals("-F")) {
-				// TODO: search username(s) via file
+				usernames = readNamesFromFile(args[1]);
 			}
 			// file is username
 			else if (args[0].trim().equals("-u") || args[0].trim().equals("-U")) {
-				// TODO: serach username via user input
+				// add username(s) to list
+				for(int i = 1; args[i] != null; i++) {
+					usernames.add(args[i].trim());
+				}
 			}
 			// malformed argument, print usage
 			else {
@@ -44,12 +51,45 @@ public class MainMenu {
 			// split by 'space' into list of username(s)
 			usernames = (ArrayList<String>) Arrays.asList(input.split("\\s+"));
 			// append newline
-			System.out.print("\n");
-			// process all usernames in the list
-			// TODO: process
-		}		
+			System.out.print("\n");			
+			// close scanner
+			sc.close();
+		}
+		// if usernames were entered, and not errors occurred, continue
+		if(usernames.size() > 0) {
+			// TODO: scrape information...
+		}
 	}
 	
+	// read in username(s) from user specified file
+	private static ArrayList<String> readNamesFromFile(String file) {
+		
+		File inFile = new File(file.trim());					// file to read in
+		ArrayList<String> usernames = new ArrayList<String>();	// usernames extracted from file
+		
+		// attempt to open the file and create a buffered reader
+		try (BufferedReader br = new BufferedReader(new FileReader(inFile))) {
+		    // line per each read cycle
+			String line;
+			// process each line until EOF
+		    while ((line = br.readLine()) != null) {
+		    	// if more than one 'word' per line, only read first entry
+		    	usernames.add(line.split("\\s+")[0].trim());
+		    }
+		}
+		// file could not be opened/found
+		catch (FileNotFoundException e) {
+			System.out.println("File not found: " + file.toString());
+		} 
+		// error while reading in file
+		catch (IOException e) {
+			System.out.println("Error reading file: " + file.toString());
+		}
+		// return list of usernames
+		return usernames;
+	}
+
+	// print out a message to show user usages
 	private static void printUsage() {
 		System.out.println("sus\n" + 
 				"sus -u [username]\n" + 
